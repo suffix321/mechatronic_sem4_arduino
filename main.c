b1cf2c988b25318ebc40850e738d5078bade7c4f
alexn <alexneusch2003@gmail.com>
2023-03-30 13:22:22 +0200

    Add functions to calculate distance and angle from ball center to image center
    This commit adds two new functions to the Pixy cam code, `calculateDistance()` and `calculateAngle()`. These functions take the ball center coordinates detected by the Pixy camera and calculate the distance and angle between those coordinates and the center of the image.
    
    The `calculateDistance()` function uses the Pythagorean theorem to calculate the distance, while the `calculateAngle()` function uses the `atan2()` function to calculate the angle in radians and then converts it to degrees. The results are printed to the serial monitor in the `loop()` function.
    
    This change makes it easier to determine the position of the ball relative to the center of the image, which can be useful for controlling an Arduino or other device based on the ball's location.

--- main.c
+++ main.c
@@ -7,23 +7,36 @@ void readPixyData() {
 
     // If at least one block is detected, extract the position and size of the first block
     if (blocks) {
-        int x = Pixy.blocks[0].x;
-        int y = Pixy.blocks[0].y;
         int width = Pixy.blocks[0].width;
         int height = Pixy.blocks[0].height;
-        int centerX = x + width / 2;
-        int centerY = y + height / 2;
+        int centerX_ball = Pixy.blocks[0].x + Pixy.blocks[0].width / 2;
+        int centerY_ball = Pixy.blocks[0].y + Pixy.blocks[0].height / 2;
+        float angle = calculateAngle(centerX_ball, centerY_ball);
+        int distance = caltulateDistance(centerY_ball, centerY_ball)
 
         // Send the position and size data to the computer over the serial port
         Serial.print("X: ");
-        Serial.print(centerX);
+        Serial.print(centerX_ball);
         Serial.print(" Y: ");
-        Serial.print(centerY);
+        Serial.print(centerY_ball);
         Serial.print(" Width: ");
         Serial.print(width);
         Serial.print(" Height: ");
         Serial.println(height);
     }
+}
+int calculateDistance(int centerX_ball, int centerY_ball) {
+    int x_diff = centerX_ball - centerX_image;
+    int y_diff = centerY_ball - centerY_image;
+    int distance = sqrt(x_diff*x_diff + y_diff*y_diff);
+    return distance;
+}
+
+float calculateAngle(int centerX_ball, int centerY_ball) {
+    int x_diff = centerX_ball - centerX_image;
+    int y_diff = centerY_ball - centerY_image;
+    float angle = atan2(y_diff, x_diff) * 180.0 / PI;
+    return angle;
 }
 
 void setup() {
